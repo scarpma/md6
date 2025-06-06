2ddb0d63314fd9223fbf4893fd875cb2c3ad2aac
scarpma <scarpma@gmail.com>
2025-06-06 10:18:19 +0200

    changed comments from italian to english

--- main.c
+++ main.c
@@ -6,27 +6,25 @@
 
 int main() {
 
-  // APRO FILE DEI PARAMETRI
+  // OPEN PARAMETER FILE
   param = fopen("./param.in","r");
   if (!param) {
     perror("fopen param");
     exit(1);
   }
-
-  // CARICO PARAMETRI DA PARAM.IN
+  // READ PARAMETERS
   fscanf(param,"npartx=%i\nnparty=%i\nnlayers=%i\nnpart=%i\nwrite_jump=%i\ntimesteps=%i\ndt=%g\neps=%g\nsigma=%g\nmu=%g\nvar=%g\nm=%g\na_lattice=%g\npot_trunc_perc=%g\nnew_in_cond=%i",&npartx,&nparty,&nlayers,&npart,&write_jump,&timesteps,&dt,&eps,&sigma,&mu,&var,&m,&a_lattice,&pot_trunc_perc,&newc);
   fclose(param);
-  
-  // INIZIALIZZO VARIABILI DA PARAMETRI
+  // INITIALIZE VARIABLES
   dtdouble = 2.*dt;
   dtsquare = pow(dt, 2.);
-  r_max = sigma * pow((1+sqrt( 1-16*pot_trunc_perc ))/( 2*pot_trunc_perc ), 1./6.);  /*impostando la percentuale di troncamento del potenziale, posso calcolare rmax sapendo che è il valore in cui il potenziale raggiunge quella percentuale impostata */
+  r_max = sigma * pow((1+sqrt( 1-16*pot_trunc_perc ))/( 2*pot_trunc_perc ), 1./6.);  /*r_max IS COMPUTED BASED ON pot_trunc_perc, i.e. when POTENTIAL REACHES pot_trunc_perc OF ITS MAX VALUE*/
   r_max_squared = pow(r_max, 2.);
-  shift = potenergy(r_max, eps, sigma); // per lo shift del potenziale
-  BOXL = ( 0.5 + nlayers ) * a_lattice; // (0.5 + max(npartx,nparty,nlayers) * a_lattice è la lunghezza  del reticolo nella direzione che la rende massima)
+  shift = potenergy(r_max, eps, sigma); // POTENTIAL SHIFT
+  BOXL = ( 0.5 + nlayers ) * a_lattice; // (0.5 + max(npartx,nparty,nlayers) * a_lattice IS THE LATTICE LENGHT IN EACH DIRECTION)
   reduced_density = npart * sigma / pow(BOXL, 3.);
 
-  // VEDO SE CONTINUARE O INIZIARE UNA NUOVA SIMULAZIONE
+  // RESTART OLD SIMULATION OR START FROM SCRATCH
   if (newc == 0) {
     logfile = fopen("./salvati/run.log", "a");
     if (!logfile) {
@@ -40,9 +38,9 @@ int main() {
     printf("Proseguo ultima simulazione. nrun = %d, t_in = %.2f\n",nrun,last_durata_totale);
     fprintf(logfile,"Proseguo ultima simulazione. nrun = %d, t_in = %.2f\n",nrun,last_durata_totale);
   } else if (newc == 1) {
-    int status = system("./del-data"); // esegue uno script di sistema
-    status = system("mkdir ./salvati"); // esegue uno script di sistema
-    status = system("mkdir ./data"); // esegue uno script di sistema
+    int status = system("./del-data");
+    status = system("mkdir ./salvati");
+    status = system("mkdir ./data");
     printf("r_max=%g    BOXL=%g    red. dens=%g\n",r_max,BOXL,reduced_density);
     nrun = 0;
     last_durata_totale = 0.;
@@ -51,21 +49,21 @@ int main() {
       perror("fopen logfile");
       exit(1);
     }
-    printf("Inizio una nuova simulazione: inizializzo reticolo FCC e velocità random.\n");
-    fprintf(logfile,"Inizio una nuova simulazione:\n\nPARAM:\n");
+    printf("Initialize FCC lattice and random velocities.\n");
+    fprintf(logfile,"start new simulation:\n\nPARAM:\n");
     fprintf(logfile,"npartx=%i\nnparty=%i\n",npartx,nparty);
     fprintf(logfile,"nlayers=%i\nnpart=%i\n",nlayers,npart);
     fprintf(logfile,"write_jump=%i\ntimesteps=%i\n",write_jump,timesteps);
     fprintf(logfile,"dt=%g\neps=%g\nsigma=%g\n",dt,eps,sigma);
     fprintf(logfile,"mu=%g\nvar=%g\nm=%g\na_lattice=%g\n",mu,var,m,a_lattice);
     fprintf(logfile,"pot_trunc_perc=%g\nnew_in_cond=%i\n\n\n",pot_trunc_perc,newc);
-    fprintf(logfile,"inizializzo reticolo FCC e velocità random\n\n");
+    fprintf(logfile,"Initialize FCC lattice and random velocities\n\n");
     fprintf(logfile, "r_max=%g    BOXL=%g    red. dens=%g\n",r_max,BOXL,reduced_density);
     fcc();
   }
     
     
-  // APRO FILE OUTPUT E LI INIZIALIZZO
+  // OPEN OUTPUT FILES AND WRITE HEADERS
   stat = fopen("./data/stat.dat","w");
   if (!stat) {
     perror("fopen stat");
@@ -74,14 +72,14 @@ int main() {
   fprintf(stat,"# t(0)   sumvx(1)   sumvy(2)   sumvz(3)    kenergy(4)   penergy(5)   energy(6)   red_temp(7)\n");
   output = fopen("./data/verlet_periodic.xyz","w");
     
-  // DICHIARO VARIABILI DINAMICHE
+  // DECLARE VARIABLES
   vec r[npart], ro[npart], a[npart];
   
-  // CARICO CONDIZIONI INIZIALI E GENERO LE ro
+  // LOAD INITIAL CONDITIONS
   load_r(r);
   write_r(output, r);
-  printf("Inizio ad integrare:\n\n");
-  fprintf(logfile, "Inizio ad integrare: attendi!\n\n");
+  printf("Integration started:\n\n");
+  fprintf(logfile, "Integration started: wait!\n\n");
     for (int i = 0; i < npart; i++) {
         a[i].x = 0.0;
         a[i].y = 0.0;
@@ -92,17 +90,17 @@ int main() {
   write_r(output, r);
 
 
-  // CONTROLLO CHE TUTTE LE PARTICELLE SIANO NEL BOX
+  // CHECK PARTICLES INSIDE BOX
   int out_count = 0;
   for (int i = 0; i < npart; i++) {
     if (r[i].x < -BOXL/2. || r[i].x > BOXL/2.) out_count += 1;
     if (r[i].y < -BOXL/2. || r[i].y > BOXL/2.) out_count += 1;
     if (r[i].z < -BOXL/2. || r[i].z > BOXL/2.) out_count += 1;
   }
-  if (out_count > 0) printf("Le condizioni iniziali non sono all'interno del box periodico.\n");
+  if (out_count > 0) printf("initial conditions are not inside periodic box.\n");
 
 
-  // INTEGRO CON VERLET
+  // VERLET INTEGRATION
   t = 2;
   for (int tt = 2; tt < (timesteps-1) / write_jump; tt++) {
     for (int k = 0; k < write_jump; k++) {
@@ -115,20 +113,20 @@ int main() {
     t++;
   }
   
-  // ULTIMA INTEGRAZIONE
+  // LAST TIME STEP
   output_in_cond = fopen("./data/in_cond.xyz","w");
     output_in_cond_vel = fopen("./data/in_cond_vel.dat","w");
   compute_forces_stat(r, a);
     verlet_periodic_last(r, ro, a); t++;
   write_durata_totale();
   
-  // CHIUDO FILES
+  // CLOSE FILES
   fclose(output);
   fclose(output_in_cond);
   fclose(output_in_cond_vel);
   fclose(stat);
-  printf("Finito!\n\n");
-  fprintf(logfile, "Finito!\n\n\n\n\n\n\n\n");
+  printf("Done!\n\n");
+  fprintf(logfile, "Done!\n\n\n\n\n\n\n\n");
   fclose(logfile);
 }
 
