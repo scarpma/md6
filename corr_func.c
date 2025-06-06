d69d32118e5bf74fe2da94c379b315b848d2c0f8
scarpma <scarpma@gmail.com>
2025-06-06 12:19:00 +0200

    added checks for files fixed corr_func file opening fixed totduration file opening

--- corr_func.c
+++ corr_func.c
@@ -13,10 +13,16 @@ int main(int argc, char *argv[]) {
   // APRO FILES E LI INIZIALIZZO
   snprintf(paramfilepath,         STRLEN, "%s/param.in",            argv[1]);
   snprintf(coordfilepath,         STRLEN, "%s/verlet_periodic.xyz", argv[1]);
-  snprintf(autodiffusionfilepath, STRLEN, "%s/autodiffusion.dat",   argv[1]);
+  snprintf(corrfuncfilepath,      STRLEN, "%s/corr_func.dat",   argv[1]);
+  snprintf(totdurationfilepath,   STRLEN, "%s/tot_duration.dat",    argv[1]);
   paramfile        = fopen(paramfilepath,            "r");
   coordfile        = fopen(coordfilepath,            "r");
   corrfuncfile     = fopen(corrfuncfilepath,         "w");
+  totdurationfile   = fopen(totdurationfilepath, "r");
+  CHECK_FILE(paramfile,        paramfilepath);
+  CHECK_FILE(coordfile,        coordfilepath);
+  CHECK_FILE(corrfuncfile,     corrfuncfilepath);
+  CHECK_FILE(totdurationfile,  totdurationfilepath);
 
   // READ PARAMETERS
   fscanf(paramfile,"npartx=%i\nnparty=%i\nnlayers=%i\nnpart=%i\nwrite_jump=%i\ntimesteps=%i\ndt=%g\neps=%g\nsigma=%g\nmu=%g\nvar=%g\nm=%g\na_lattice=%g\npot_trunc_perc=%g\nnew_in_cond=%i",&npartx,&nparty,&nlayers,&npart,&write_jump,&timesteps,&dt,&eps,&sigma,&mu,&var,&m,&a_lattice,&pot_trunc_perc,&newc);
