6b6ac66671469bb45ba2eeb2fc915cebe266f5f1
scarpma <scarpma@gmail.com>
2025-06-06 13:53:44 +0200

    main, corr_func, autodiffusion. analysis and open plots in pdf

--- run.sh
+++ run.sh
@@ -1,28 +1,11 @@
 #!/bin/bash
 
-STARTTIME=$(date +%s)
-./exec/main &&
-./exec/autodiffusion &&
-python3 graph/stat.py;
-python3 graph/autodiffusion.py;
-#gnuplot graph/autodiffusion.gnu;
+RUN_DIR=$1
 
-
-input="./data/durata_totale.dat";
-nrun=$(awk 'NR==2' $input);
-stat_final_path=$(printf "./salvati/stat%s.pdf" $nrun)
-autodiffusion_final_path=$(printf "./salvati/autodiffusion%s.pdf" $nrun)
-stat_dat_final_path=$(printf "./salvati/stat%s.dat" $nrun)
-autodiffusion_dat_final_path=$(printf "./salvati/autodiffusion%s.dat" $nrun)
-
-cp "./data/stat.pdf" $stat_final_path;
-cp "./data/autodiffusion.pdf" $autodiffusion_final_path;
-cp "./data/stat.dat" $stat_dat_final_path;
-cp "./data/autodiffusion.dat" $autodiffusion_dat_final_path;
-
-ENDTIME=$(date +%s)
-echo "run time = $(($ENDTIME-$STARTTIME)) s" >> ./salvati/run.log
-printf "run time = $(($ENDTIME-$STARTTIME)) s\n";
-open $stat_final_path &
-open $autodiffusion_final_path &
+(./exec/main $RUN_DIR &&
+./exec/autodiffusion $RUN_DIR  &&
+./exec/corr_func $RUN_DIR  &&
+python3 graph/stat.py $RUN_DIR &&
+python3 graph/autodiffusion.py $RUN_DIR) &&
+(open $RUN_DIR/autodiffusion.pdf; open $RUN_DIR/stat.pdf)
 #vmd -e /home/scarpma/md6/graph/view.vmd
