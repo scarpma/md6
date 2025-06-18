baa63a58f7097896dd79bcf5016b866dd2debdbc
scarpma <scarpma@gmail.com>
2025-06-18 17:25:33 +0200

    no output during tests

--- run_tests.sh
+++ run_tests.sh
@@ -5,5 +5,5 @@ for dir in test_*/; do
   echo "Doing test ${dir}"
   echo "==========================="
   echo ""
-  ./exec/main $dir && (cd $dir && python3 analyze.py)
+  ./exec/main $dir > $dir/log.txt && (cd $dir && python3 analyze.py)
 done
